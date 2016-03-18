b8c7606c6ae40bfa71f4158fb8e2d1986d93f6e1
Badr Tazi <tasibadr@gmail.com>
2016-03-18 21:51:38 +0000

    fixed average price for each brand's toys

--- lib/app.rb
+++ lib/app.rb
@@ -19,25 +19,32 @@ puts "|_|                                       "
 products_hash["items"].each do |toy|
 	# Print the name of the toy
 	puts "name: #{toy["title"]}"
+
 	# Print the retail price of the toy
 	retail_price = toy["full-price"]
 	puts "retail price: #{retail_price}"
+
 	# Calculate and print the total number of purchases
 	number_of_purchases = toy["purchases"].length
 	puts "number of purchases: #{number_of_purchases}"
+
 	# Calculate and print the total amount of sales
 	sales = 0
 	toy["purchases"].each do |p|
 		sales += p["price"]
 	end
 	puts "sales: #{sales}"
+
 	# Calculate and print the average price the toy sold for
 	average_price = sales/number_of_purchases
 	puts "average_price: #{average_price}"
+
 	# Calculate and print the average discount (% or $) based off the average sales price
 	average_discount = (((retail_price.to_f - average_price.to_f)/retail_price.to_f)*100).round(2)
 	puts "average discount: #{average_discount}%"		
+
 	puts "----------------------------------"
+
 end
 
 
@@ -56,11 +63,13 @@ brands.each do |brand|
 	stock = 0
 	average = 0
 	revenue = 0
+	brand_toys = 0
 	toys = products_hash["items"]
 	toys.each do |toy|
 		if toy["brand"] == brand
 			stock += toy["stock"]
 			average += toy["full-price"].to_f
+			brand_toys += 1
 			toy["purchases"].each do |purchase|
 				revenue += purchase["price"]
 			end
@@ -72,7 +81,7 @@ brands.each do |brand|
 	# Count and print the number of the brand's toys we stock
 	puts "stock: #{stock}"
 	# Calculate and print the average price of the brand's toys
-	average = (average/toys.size).round(2)
+	average = (average/brand_toys).round(2)
 	puts "average price: #{average}"
 	# Calculate and print the total revenue of all the brand's toy sales combined
 	puts "revenue: #{revenue.round(2)}$"	
